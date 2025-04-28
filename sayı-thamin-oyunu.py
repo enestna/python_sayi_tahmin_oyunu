e0635c35242a31fdd4f3f58aaf3c76cfd2bb718e
Enes <enestuna2531@gmail.com>
2025-04-28 17:53:21 +0300

    sayı-thamin-oyunu.py

--- sayı-thamin-oyunu.py
+++ sayı-thamin-oyunu.py
@@ -5,21 +5,32 @@ def play():
     guess=None
     point=100
     print("Guess a number between 1 and 100")
-
+    attempt_count=0
     while guess!=correct_number:
         guess=int(input("Please enter a guess number"))
-        if guess<correct_number and guess>=1 and guess<=100:
-            print("please enter a larger number")
-            point-=10
-        elif guess>correct_number and guess>=1 and guess<=100:
-            print("please enter a smaller number")
-            point-=10
-        else:
+        if guess<1 or guess>100:
             print("please enter a number between 1 and 100")
+            continue
+        if guess<correct_number :
+            print("please enter a larger number: ")
+            point-=10
+            attempt_count+=1
+
+        elif guess>correct_number :
+            print("please enter a smaller number: ")
+            point-=10
+            attempt_count+=1
+
+       
         if point==0:
             print("Sorry, you have no points left!")
+            print(f"your {attempt_count}. try")
+
             break
-    print("Congratulations the number is correct")
-    print("your point",point)
+        if guess==correct_number:
+                attempt_count+=1
+                print("Congratulations the number is correct")
+                print(f"your {attempt_count}. try")
+                print("your point=",point)
 
 play()
